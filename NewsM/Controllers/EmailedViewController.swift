5687b084d4fe7a293647b074d8672ca66c0c6347
MK1 <shin.m.dp@gmail.com>
2023-08-21 11:44:37 +0300

    add emailedViewFunc

--- NewsM/Controllers/EmailedViewController.swift
+++ NewsM/Controllers/EmailedViewController.swift
@@ -7,11 +7,85 @@
 
 import Foundation
 import UIKit
+import SnapKit
 
-class EmailedViewController: UIViewController {
-    
+class EmailedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    //MARK: Properties
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "YourCellIdentifier")
+        return tableView
+    }()
+    var articles: [[String: Any]] = []     // Создайте массив для хранения данных
+    //MARK: Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        view?.backgroundColor = .systemBlue
+        view.backgroundColor = .systemBlue
+        setupTableViewConstraints()
+        fetchEmailedArticles() // call method api
     }
-}
+    //MARK: Methods
+    func setupTableViewConstraints() {
+        view.addSubview(tableView)
+        tableView.snp.makeConstraints { make in
+            make.top.equalToSuperview()
+            make.leading.equalToSuperview()
+            make.trailing.equalToSuperview()
+            make.bottom.equalToSuperview()
+        }
+    }
+    //MARK: numberOfRowsInSection
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return articles.count
+    }
+    //MARK: cellForRowAt
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "YourCellIdentifier", for: indexPath)
+        // Наполните ячейку данными из массива articles
+        let article = articles[indexPath.row]
+        
+        if let title = article["title"] as? String {
+            cell.textLabel?.text = title
+            cell.textLabel?.numberOfLines = 0 // Установите numberOfLines в 0 для текстовой метки
+        }
+        
+        if let publishedDate = article["published_date"] as? String {
+            cell.detailTextLabel?.text = "Дата публикации: \(publishedDate)"
+        }
+        // Другие поля, которые вас интересуют
+        return cell
+    }
+    //MARK: API
+    func fetchEmailedArticles() {
+        let apiKey = "FmT19AaabNgeLfhi0HD0pHW9NWwXcNKl"
+        let emailedURL = URL(string: "https://api.nytimes.com/svc/mostpopular/v2/emailed/30.json?api-key=\(apiKey)")
+        let session = URLSession.shared
+        let emailedTask = session.dataTask(with: emailedURL!) { [weak self] (data, response, error) in
+            guard let self = self else { return }
+            if let error = error {
+                print("Ошибка при получении данных о самых электронных статьях: \(error.localizedDescription)")
+                return
+            }
+            // разбор JSON
+            if let data = data {
+                do {
+                    if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
+                        // Вместо вывода данных в консоль, добавьте их в массив
+                        if let results = json["results"] as? [[String: Any]] {
+                            self.articles = results
+                            DispatchQueue.main.async {
+                                self.tableView.reloadData() // Обновите таблицу, когда данные будут доступны
+                            }
+                        }
+                    }
+                } catch {
+                    print("Ошибка при разборе данных JSON: \(error.localizedDescription)")
+                }
+            }
+        }
+        emailedTask.resume()
+    }
+} // end emailedViewController
--- NewsM/Managers/NewsManager.swift
+++ NewsM/Managers/NewsManager.swift
@@ -5,5 +5,78 @@
 //  Created by SHIN MIKHAIL on 20.08.2023.
 //
 // FmT19AaabNgeLfhi0HD0pHW9NWwXcNKl
+//func fetchMostPopularArticles() {
+//    let apiKey = "FmT19AaabNgeLfhi0HD0pHW9NWwXcNKl"
+//    // URL-адреса для разных категорий
+//    let emailedURL = URL(string: "https://api.nytimes.com/svc/mostpopular/v2/emailed/30.json?api-key=\(apiKey)")
+//    let sharedURL = URL(string: "https://api.nytimes.com/svc/mostpopular/v2/shared/30.json?api-key=\(apiKey)")
+//    let viewedURL = URL(string: "https://api.nytimes.com/svc/mostpopular/v2/viewed/30.json?api-key=\(apiKey)")
+//
+//    let session = URLSession.shared
+//
+//    // запрос до API New York Times.
+//    let emailedTask = session.dataTask(with: emailedURL!) { (data, response, error) in
+//        if let error = error {
+//            print("Ошибка при получении данных о самых электронных статьях: \(error.localizedDescription)")
+//            return
+//        }
+//        // Здесь можно разбирать полученные данные в формате JSON для самых электронных статей.
+//    }
+//
+//    let sharedTask = session.dataTask(with: sharedURL!) { (data, response, error) in
+//        if let error = error {
+//            print("Ошибка при получении данных о самых популярных статьях: \(error.localizedDescription)")
+//            return
+//        }
+//        // Здесь можно разбирать полученные данные в формате JSON для самых популярных статей.
+//    }
+//
+//    let viewedTask = session.dataTask(with: viewedURL!) { (data, response, error) in
+//        if let error = error {
+//            print("Ошибка при получении данных о самых просматриваемых статьях: \(error.localizedDescription)")
+//            return
+//        }
+//        // Здесь можно разбирать полученные данные в формате JSON для самых просматриваемых статей.
+//    }
+//
+//    // Запускаем задачи для получения данных.
+//    emailedTask.resume()
+//    sharedTask.resume()
+//    viewedTask.resume()
+//}
 
 import Foundation
+
+/*
+func fetchSharedArticles() {
+    let apiKey = "FmT19AaabNgeLfhi0HD0pHW9NWwXcNKl"
+    let sharedURL = URL(string: "https://api.nytimes.com/svc/mostpopular/v2/shared/1.json?api-key=\(apiKey)")
+    let session = URLSession.shared
+    
+    let sharedTask = session.dataTask(with: sharedURL!) { (data, response, error) in
+        if let error = error {
+            print("Ошибка при получении данных о самых популярных статьях: \(error.localizedDescription)")
+            return
+        }
+        // Здесь можно разбирать полученные данные в формате JSON для самых популярных статей.
+    }
+    
+    sharedTask.resume()
+}
+
+func fetchViewedArticles() {
+    let apiKey = "FmT19AaabNgeLfhi0HD0pHW9NWwXcNKl"
+    let viewedURL = URL(string: "https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?api-key=\(apiKey)")
+    let session = URLSession.shared
+    
+    let viewedTask = session.dataTask(with: viewedURL!) { (data, response, error) in
+        if let error = error {
+            print("Ошибка при получении данных о самых просматриваемых статьях: \(error.localizedDescription)")
+            return
+        }
+        // Здесь можно разбирать полученные данные в формате JSON для самых просматриваемых статей.
+    }
+    
+    viewedTask.resume()
+}
+*/
