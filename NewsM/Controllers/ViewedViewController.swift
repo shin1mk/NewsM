bb92a9e87c79dcc5daa13f17b9e50f8ceae028a7
MK1 <shin.m.dp@gmail.com>
2023-08-22 16:12:15 +0300

    add 3 tabs

--- NewsM/Controllers/ViewedViewController.swift
+++ NewsM/Controllers/ViewedViewController.swift
@@ -1,5 +1,5 @@
 //
-//  SettingsViewController.swift
+//  ViewedViewController.swift
 //  NewsM
 //
 //  Created by SHIN MIKHAIL on 20.08.2023.
@@ -7,11 +7,79 @@
 
 import Foundation
 import UIKit
+import SnapKit
 
-class ViewedViewController: UIViewController {
-    
+final class ViewedViewController: UIViewController {
+    private var articles: [NewsArticle] = []
+    //MARK: Properties
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.register(CustomTableViewCell.self, forCellReuseIdentifier: "CustomCell")
+        return tableView
+    }()
+    //MARK: Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        view?.backgroundColor = .systemYellow
+        setupTableViewConstraints()
+        fetchViewedArticles()
+    }
+    //MARK: Methods
+    private func setupTableViewConstraints() {
+        view.addSubview(tableView)
+        tableView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+    //MARK: API
+    private func fetchViewedArticles() {
+        NewsManager.shared.fetchViewedArticles { [weak self] articles, error in
+            guard let self = self else { return }
+            
+            if let error = error {
+                print("Error fetching data from the API: \(error.localizedDescription)")
+                // Обработайте ошибку, если необходимо
+            } else if let articles = articles {
+                self.articles = articles
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+                print("Parsed articles: \(articles)")
+            }
+        }
+    }
+} // end
+//MARK: TableView
+extension ViewedViewController: UITableViewDelegate, UITableViewDataSource{
+    //MARK: heightForRowAt
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 90.0
+    }
+    //MARK: numberOfRowsInSection
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return articles.count
+    }
+    //MARK: cellForRowAt
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CustomCell", for: indexPath) as! CustomTableViewCell
+        let article = articles[indexPath.row]
+        cell.backgroundColor = UIColor.black
+        // юзаем свойства структуры
+        cell.newsArticle = article
+        return cell
+    }
+    //MARK: didSelectRowAt
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        if let cell = tableView.cellForRow(at: indexPath) as? CustomTableViewCell,
+           let newsArticle = cell.newsArticle {
+            let webViewController = WebViewController()
+            webViewController.newsArticle = newsArticle // Передали экземпляр NewsArticle
+            navigationController?.pushViewController(webViewController, animated: true)
+        }
     }
 }
+
--- NewsM/Managers/NewsManager.swift
+++ NewsM/Managers/NewsManager.swift
@@ -81,63 +81,116 @@ final class NewsManager {
         }
         emailedTask.resume()
     }
+    
+    func fetchSharedArticles(completion: @escaping ([NewsArticle]?, Error?) -> Void) {
+        let apiKey = "FmT19AaabNgeLfhi0HD0pHW9NWwXcNKl"
+        let emailedURL = URL(string: "https://api.nytimes.com/svc/mostpopular/v2/shared/1.json?api-key=\(apiKey)")
+        let session = URLSession.shared
+        
+        let emailedTask = session.dataTask(with: emailedURL!) { (data, response, error) in
+            if let error = error {
+                completion(nil, error)
+                return
+            }
+            
+            if let data = data {
+                do {
+                    if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
+                        // Extract articles from the "results" array
+                        if let results = json["results"] as? [[String: Any]] {
+                            var parsedArticles: [NewsArticle] = []
+                            for result in results {
+                                if let title = result["title"] as? String,
+                                   let abstract = result["abstract"] as? String,
+                                   let url = result["url"] as? String,
+                                   let publishedDate = result["published_date"] as? String,
+                                   let mediaArray = result["media"] as? [[String: Any]] {
+                                    var mediaMetadata: [MediaMetadata] = []
+                                    for media in mediaArray {
+                                        if let metadataArray = media["media-metadata"] as? [[String: Any]] {
+                                            for metadata in metadataArray {
+                                                if let mediaURL = metadata["url"] as? String,
+                                                   let mediaFormat = metadata["format"] as? String,
+                                                   let mediaHeight = metadata["height"] as? Int,
+                                                   let mediaWidth = metadata["width"] as? Int {
+                                                    let metadata = MediaMetadata(url: mediaURL, format: mediaFormat, height: mediaHeight, width: mediaWidth)
+                                                    mediaMetadata.append(metadata)
+                                                }
+                                            }
+                                        }
+                                    }
+                                    let article = NewsArticle(title: title,
+                                                              abstract: abstract,
+                                                              url: url,
+                                                              publishedDate: publishedDate,
+                                                              mediaMetadata: mediaMetadata)
+                                    parsedArticles.append(article)
+                                }
+                            }
+                            completion(parsedArticles, nil)
+                        }
+                    }
+                } catch {
+                    completion(nil, error)
+                }
+            }
+        }
+        emailedTask.resume()
+    }
+    
+    func fetchViewedArticles(completion: @escaping ([NewsArticle]?, Error?) -> Void) {
+        let apiKey = "FmT19AaabNgeLfhi0HD0pHW9NWwXcNKl"
+        let emailedURL = URL(string: "https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?api-key=\(apiKey)")
+        let session = URLSession.shared
+        
+        let emailedTask = session.dataTask(with: emailedURL!) { (data, response, error) in
+            if let error = error {
+                completion(nil, error)
+                return
+            }
+            
+            if let data = data {
+                do {
+                    if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
+                        // Extract articles from the "results" array
+                        if let results = json["results"] as? [[String: Any]] {
+                            var parsedArticles: [NewsArticle] = []
+                            for result in results {
+                                if let title = result["title"] as? String,
+                                   let abstract = result["abstract"] as? String,
+                                   let url = result["url"] as? String,
+                                   let publishedDate = result["published_date"] as? String,
+                                   let mediaArray = result["media"] as? [[String: Any]] {
+                                    var mediaMetadata: [MediaMetadata] = []
+                                    for media in mediaArray {
+                                        if let metadataArray = media["media-metadata"] as? [[String: Any]] {
+                                            for metadata in metadataArray {
+                                                if let mediaURL = metadata["url"] as? String,
+                                                   let mediaFormat = metadata["format"] as? String,
+                                                   let mediaHeight = metadata["height"] as? Int,
+                                                   let mediaWidth = metadata["width"] as? Int {
+                                                    let metadata = MediaMetadata(url: mediaURL, format: mediaFormat, height: mediaHeight, width: mediaWidth)
+                                                    mediaMetadata.append(metadata)
+                                                }
+                                            }
+                                        }
+                                    }
+                                    let article = NewsArticle(title: title,
+                                                              abstract: abstract,
+                                                              url: url,
+                                                              publishedDate: publishedDate,
+                                                              mediaMetadata: mediaMetadata)
+                                    parsedArticles.append(article)
+                                }
+                            }
+                            completion(parsedArticles, nil)
+                        }
+                    }
+                } catch {
+                    completion(nil, error)
+                }
+            }
+        }
+        emailedTask.resume()
+    }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
- func fetchSharedArticles() {
- let apiKey = "FmT19AaabNgeLfhi0HD0pHW9NWwXcNKl"
- let sharedURL = URL(string: "https://api.nytimes.com/svc/mostpopular/v2/shared/1.json?api-key=\(apiKey)")
- let session = URLSession.shared
- 
- let sharedTask = session.dataTask(with: sharedURL!) { (data, response, error) in
- if let error = error {
- print("Ошибка при получении данных о самых популярных статьях: \(error.localizedDescription)")
- return
- }
- // Здесь можно разбирать полученные данные в формате JSON для самых популярных статей.
- }
- 
- sharedTask.resume()
- }
- 
- func fetchViewedArticles() {
- let apiKey = "FmT19AaabNgeLfhi0HD0pHW9NWwXcNKl"
- let viewedURL = URL(string: "https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?api-key=\(apiKey)")
- let session = URLSession.shared
- 
- let viewedTask = session.dataTask(with: viewedURL!) { (data, response, error) in
- if let error = error {
- print("Ошибка при получении данных о самых просматриваемых статьях: \(error.localizedDescription)")
- return
- }
- // Здесь можно разбирать полученные данные в формате JSON для самых просматриваемых статей.
- }
- 
- viewedTask.resume()
- }
- */
